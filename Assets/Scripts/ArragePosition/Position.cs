2d921a2c0e5ea1760c218578d1499b9c99399e8b
Lynrbe <khanhlinh26004@gmail.com>
2024-05-14 20:03:15 +0700

    update arrage position

--- Assets/Scripts/ArragePosition/Position.cs
+++ Assets/Scripts/ArragePosition/Position.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class Position : MonoBehaviour
 {
-    public GameObject disk;
+    public GameObject[] disk;
+    public Sprite[] S_Object;
+    public static bool m_created = false;
+
+
+    public List<int> TakeList = new List<int>();
+    private int randomNumber;
     // Start is called before the first frame update
     void Start()
     {
         // GameObject newdisk = Instantiate(disk);
-        CreateDisk(3);
+        CreateDisk(4);
     }
 
     void CreateDisk(int num)
     {
-        for (int i=0; i<num; i++)
+
+        TakeList = new List<int>(new int[disk.Length]);
+        for (int i = 0; i < disk.Length; i++)
         {
-            //GameObject newdisk = Instantiate(disk, new Vector3(i, disk.transform.position.y, i), disk.transform.position);
+            GameObject newdisk = UnityEngine.Object.Instantiate(disk[i]);
+           // randomNumber = UnityEngine.Random.Range(0, (S_Object.Length));
+         
+           //TakeList[i] = randomNumber;
+            disk[i].GetComponent<SpriteRenderer>().sprite = S_Object[TakeList[i]];
         }
+        
     }
 }
