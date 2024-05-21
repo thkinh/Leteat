cf31881e2a8903411932592e71779d3f683300f5
Lynrbe <khanhlinh26004@gmail.com>
2024-05-21 09:15:33 +0700

    update scene arrageposition p2

--- Assets/Scripts/ArragePosition/Position.cs
+++ Assets/Scripts/ArragePosition/Position.cs
@@ -9,6 +9,7 @@ using UnityEngine.SceneManagement;
 public class Position : MonoBehaviour
 {
     public GameObject disk;
+    public Sprite[] S_Object;
     public List<GameObject> TakeList = new List<GameObject>();
     public List<Transform> availablePositions = new List<Transform>();
     private void Start()
@@ -34,6 +35,27 @@ public class Position : MonoBehaviour
         else
         {
             Debug.LogError("Object prefab or spawn position is not assigned.");
+        }
+
+    }
+    public void AddObjectToList(GameObject obj)
+    {
+        if (!TakeList.Contains(obj))
+        {
+            TakeList.Add(obj);
+
+            Debug.Log($"{obj.name} has been added to the list.");
+
+            // In ra danh sách đối tượng hiện tại (chỉ để kiểm tra)
+            Debug.Log("Current objects in list:");
+            foreach (GameObject objInList in TakeList)
+            {
+                Debug.Log(objInList.name);
+            }
+        }
+        else
+        {
+            Debug.Log($"{obj.name} is already in the list.");
         }
     }
 }
