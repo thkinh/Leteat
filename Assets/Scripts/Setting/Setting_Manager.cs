31a4605096cb6517318851057836045bbf8f6cc5
linkieee <geniegialinh@gmail.com>
2024-06-12 01:13:42 +0700

    add home button, fix music

--- Assets/Scripts/Setting/Setting_Manager.cs
+++ Assets/Scripts/Setting/Setting_Manager.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Setting_Manager : MonoBehaviour
 {
     public GameObject Exit_btn;
     public GameObject Audio_Toggle;
-    public TMP_InputField ServerIP;
-    
+    [SerializeField] private Slider volumeSlider;
+
     public void Exit()
     {
         SceneManager.LoadScene("Menu");
     }
 
+    void Start()
+    {
+        if (SoundManager.instance != null && volumeSlider != null)
+        {
+            SoundManager.instance.SetVolumeSlider(volumeSlider);
+        }
+    }
 }
